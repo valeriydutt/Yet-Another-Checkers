9fd7103e3e8aafba3d21bfab63aecfa8f808d820
Sultan Tanirkul <tanirkul.sultan@gmail.com>
2020-04-19 15:55:29 +0600

    Add move functionality and rules

--- Scene/game.gd
+++ Scene/game.gd
@@ -21,19 +21,20 @@ const CENTRE_COEF  = (1 - PIECE_SIZE / TILE_SIZE) / 2
 func _ready():
 	init_board()
 	generate_pieces()
-	
+	move(BOARD[23].piece, BOARD[30].coordinate)
+
 func generate_pieces() -> void:
 	for count in range(BOARD.size()):
 		if BOARD[count].havePiece:
 			BOARD[count].piece = load("res://Prefabs/BoardTile.tscn").instance()
 			
-			add_child(BOARD[count].piece)
-			
 			if count < BOARD.size() / 2:
 				BOARD[count].piece.set_tile_pos(BOARD[count].coordinate, BOARD[count].piece.red_piece_tex)
 			else:
 				BOARD[count].piece.set_tile_pos(BOARD[count].coordinate, BOARD[count].piece.gray_piece_tex)
-	
+			
+			add_child(BOARD[count].piece)
+
 func init_board() -> void:
 	for count in range(BOARD.size()):
 		if BOARD[count] == 1:
@@ -50,4 +51,15 @@ func init_board() -> void:
 			}
 
 func move(piece, new_position) -> void:
-	piece.set_position(new_position)
+	# Check if there is a piece
+	if piece == null:
+		return
+	# Check if tile has a piece
+	if(BOARD[(new_position.x - 2) / 32 + ((new_position.y - 2) / 32) * 8].havePiece == true):
+		return
+	# Check if move is possible
+	if (abs(new_position.x - piece.get_tile_pos().x) == TILE_SIZE &&
+			new_position.y - piece.get_tile_pos().y == TILE_SIZE): 
+		piece.set_position(new_position)
+		
+
